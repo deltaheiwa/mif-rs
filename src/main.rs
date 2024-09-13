2ff9497fd093e3e1f425f90a8765559d2d86abc6
delta <deruta.heiwa@gmail.com>
2024-09-13 17:16:27 +0200

    Ping command now returns latency

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,16 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[poise::command(slash_command, prefix_command)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("Pong!").await?;
+    let runners = &ctx.framework().shard_manager.runners.lock().await;
+    let runner_info = runners.get(&serenity::ShardId(0)).unwrap();
+
+    // Attempt to retrieve latency
+    if let Some(latency) = runner_info.latency {
+        ctx.reply(format!("Pong! `{}ms`", latency.as_millis())).await?;
+    } else {
+        // If latency is unavailable (shard just connected, and there was no heartbeat from discord yet)
+        ctx.reply("Pong!").await?;
+    }
     Ok(())
 }
 
